ecf61441e2f5e7765fc852fcbdc7a406a843151b
MJ Berends <mjr.berends@gmail.com>
2023-08-19 17:16:32 -0500

    update data loader

--- src/seedtray/data_loader.py
+++ src/seedtray/data_loader.py
@@ -23,25 +23,37 @@ MIME_TYPES = [
   TEXT_HTML,
 ]
 
-def load_data(data_name, data_path, data_download=True, mimetype=None, loader=None):
-  if mimetype is None and loader is None:
+def load_data(data_name, data_path, data_download=True, data_transforms=None, mimetype=None, data_loader=None):
+  if mimetype is None and data_loader is None:
     raise ValueError('Either mimetype or loader must be specified')
-  if mimetype == APPLICATION_TORCHVISION or loader == 'torchvision':
-    return TorchvisionLoader(data_name, data_path, data_download).load()
+  if mimetype == APPLICATION_TORCHVISION or data_loader == 'torchvision':
+    return TorchvisionLoader(
+      data_name,
+      data_path,
+      data_download,
+      data_transforms=[d.func for d in data_transforms]
+    ).load()
   else:
     raise ValueError('Unsupported mimetype or loader: {}'.format(mimetype))
 
+
 class TorchvisionLoader:
-  def __init__(self, data_name, data_path, data_download=True):
+  def __init__(self, data_name, data_path, data_download=True, data_transforms=None):
     self.data_name = data_name
     self.data_path = data_path
     self.data_download = data_download
+    self.data_transform = data_transforms[0] if len(data_transforms) > 0 else None
 
   def load(self):
     print('importing module torchvision.datasets.{}'.format(self.data_name))
     data_module = import_module('torchvision.datasets')
     data_cls = getattr(data_module, self.data_name)
-    train_data = data_cls(root=self.data_path, train=True, download=self.data_download)
+    train_data = data_cls(
+      root=self.data_path,
+      train=True,
+      download=self.data_download,
+      transform=self.data_transform
+    )
     test_data = data_cls(root=self.data_path, train=False, download=self.data_download)
     print(self.stats(train_data))
     return train_data, test_data
